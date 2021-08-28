474b0515a1957341984768184929e4289f6246e4
boonddiga <boonddiga00@naver.com>
2021-08-28 14:44:20 +0000

    Add Webpack for SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
 	entry: "./src/client/js/main.js",
 	mode: "development",
+	watch: true,
+	plugins: [new MiniCssExtractPlugin({
+		filename: "css/style.css",
+	})],
 	output: {
-		filename: "main.js",
-		path: path.resolve(__dirname, "assets", "js"),
+		filename: "js/main.js",
+		path: path.resolve(__dirname, "assets"),
+		clean: true,
 	},
 	module: {
 		rules: [
 			{
-				test: /\.js$/
-			},
-			{
+				test: /\.js$/,
 				use: {
 					loader: 'babel-loader',
 					 options: {
@@ -21,7 +25,11 @@ module.exports = {
          			 	]
 					 },
 				},
-			}
+			},
+			{	
+				test: /\.scss$/,
+        		use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+			},
 		],
 	}
 }
